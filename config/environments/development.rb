139f98745d8d4e622d8abaa8a075b3c79680eb62
sondigaraparth <sondigaraparth@gmail.com>
2022-10-29 23:07:30 +0530

    Added mailer for meeting mail send

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,6 +59,10 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  config.action_mailer.delivery_method = :letter_opener
+  
+  config.action_mailer.perform_deliveries = true
+
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Raises error for missing translations.
